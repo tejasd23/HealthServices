fe52b3a4c7359f524ee33d45e3d9e4fa81ef304b
tejasd23 <tejas.dagale023@gmail.com>
2025-11-29 02:17:43 +0530

    new setup

---
+++ healthprovider-service/src/main/java/com/example/healthprovider/healthprovider_service/HealthproviderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.healthprovider.healthprovider_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthproviderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthproviderServiceApplication.class, args);
+	}
+
+}
