a2ca5a26af91e246ea06d52c18c76645aaecab06
tejasd23 <tejas.dagale023@gmail.com>
2025-11-29 02:54:17 +0530

    new

--- healthprovider-service/src/main/java/com/example/healthprovider/healthprovider_service/HealthproviderServiceApplication.java
+++ healthprovider-service/src/main/java/com/example/healthprovider/healthprovider_service/HealthproviderServiceApplication.java
@@ -2,7 +2,9 @@ package com.example.healthprovider.healthprovider_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
+@Component
 @SpringBootApplication
 public class HealthproviderServiceApplication {
 
